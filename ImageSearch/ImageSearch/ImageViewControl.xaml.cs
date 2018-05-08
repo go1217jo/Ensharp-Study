a7d0d4f38dec5003294afb8a2190e84b6e05a264
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-05-08 19:16:35 +0900

    init and add search function

---
+++ ImageSearch/ImageSearch/ImageViewControl.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+
+namespace ImageSearch
+{
+   /// <summary>
+   /// ImageViewControl.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class ImageViewControl : UserControl
+   {
+      public ImageViewControl()
+      {
+         InitializeComponent();
+      }
+
+      private void Btn_back_Click(object sender, RoutedEventArgs e)
+      {
+
+      }
+
+      public void Btn_search_Click(object sender, RoutedEventArgs e)
+      {
+         HttpGet(txtSearchBox.Text, 10);
+         //BitmapImage bitmap = new BitmapImage(new Uri());
+         //Image image = new Image();
+         //image.Source = bitmap;
+
+         //viewPanel.Children.Add(image);
+      }
+      
+      public bool HttpGet(string POI, int count)
+      {
+         string app_key = "bfad53875abbf0b4112cb53a3c24e202";
+         string header = "KakaoAK " + app_key;
+
+         string Url = string.Format("https://dapi.kakao.com/v2/search/image");
+         StringBuilder getParams = new StringBuilder();
+         getParams.Append("?query=" + POI);
+
+         HttpWebRequest wReqFirst = (HttpWebRequest)WebRequest.Create(Url+getParams+"size="+count);
+         wReqFirst.Headers.Add("Authorization", header);
+         wReqFirst.ContentType = "application/json; charset=utf-8";
+         wReqFirst.Method = "GET";
+         wReqFirst.ServicePoint.Expect100Continue = false;
+
+         HttpWebResponse wRespFirst = (HttpWebResponse)wReqFirst.GetResponse();
+         Stream respPostStream = wRespFirst.GetResponseStream();
+         StreamReader reader = new StreamReader(respPostStream, Encoding.GetEncoding("EUC-KR"), true);
+         string responseFromServer = reader.ReadToEnd();
+
+         AddSearchedImage(responseFromServer);
+
+        // MessageBox.Show(responseFromServer);
+
+         reader.Close();
+         respPostStream.Close();
+         wRespFirst.Close();
+
+         return true;
+      }
+
+      public void AddSearchedImage(string responseFromServer)
+      {
+         var json = JObject.Parse(responseFromServer);
+
+      }
+   }
+
+
+}
---
+++ ImageSearch/ImageSearch/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageSearch
+{
+   /// <summary>
+   /// MainWindow.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      MainControl mainControl = new MainControl();
+      ImageViewControl imageViewControl = new ImageViewControl();
+
+      public MainWindow()
+      {
+         InitializeComponent();
+         MainGrid.Children.Add(mainControl);
+
+         mainControl.btn_image.Click += new RoutedEventHandler(Btn_image_Click);
+         mainControl.btn_recent.Click += Btn_recent_Click;
+         imageViewControl.btn_back.Click += Btn_back_Click;
+      }
+
+      private void Btn_image_Click(object sender, RoutedEventArgs e)
+      {
+         MainGrid.Children.Clear();
+         MainGrid.Children.Add(imageViewControl);
+      }
+
+      private void Btn_recent_Click(object sender, RoutedEventArgs e)
+      {
+         
+      }
+
+      private void Btn_back_Click(object sender, RoutedEventArgs e)
+      {
+         MainGrid.Children.Clear();
+         MainGrid.Children.Add(mainControl);
+      }
+   }
+}
