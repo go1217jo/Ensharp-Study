b4585544819c31a351638676c40ad2ac030e4309
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-05-16 15:07:14 +0900

    Print comma in number

---
+++ Calculator/Calculator/IOProcessor/Outprocessor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+   class Outprocessor
+   {
+      public string InsertComma(string number)
+      {
+         int commaCount = number.Length / 3;
+         int initCommaIndex = number.Length - commaCount * 3;
+
+         if (initCommaIndex == 0)
+            initCommaIndex += 3;
+
+         string returnNumber = number.Substring(0, initCommaIndex);
+
+         if (number.Length <= 3)
+            return number;
+
+         for(int i=initCommaIndex; i<number.Length;i+=3)
+         {
+            returnNumber += ',';
+            returnNumber += number.Substring(i, 3);
+         }
+
+         return returnNumber;
+      }
+   }
+}
--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,14 +20,18 @@ namespace Calculator
    /// </summary>
    public partial class MainWindow : Window
    {
+      string currentNumber = "";
+
+      Outprocessor outprocessor = new Outprocessor();
       public MainWindow()
       {
          InitializeComponent();
+        
       }
 
       private void Btn_percentage_Click(object sender, RoutedEventArgs e)
       {
-
+         
       }
 
       private void Btn_root_Click(object sender, RoutedEventArgs e)
@@ -67,17 +71,20 @@ namespace Calculator
 
       private void Btn_seven_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '7';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_eight_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '8';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_nine_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '9';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_multiply_Click(object sender, RoutedEventArgs e)
@@ -87,17 +94,20 @@ namespace Calculator
 
       private void Btn_four_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '4';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_five_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '5';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_six_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '6';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_minus_Click(object sender, RoutedEventArgs e)
@@ -107,17 +117,20 @@ namespace Calculator
 
       private void Btn_one_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '1';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_two_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '2';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_three_Click(object sender, RoutedEventArgs e)
       {
-
+         currentNumber += '3';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
       }
 
       private void Btn_plus_Click(object sender, RoutedEventArgs e)
@@ -126,6 +139,22 @@ namespace Calculator
       }
 
       private void Btn_plusMinus_Click(object sender, RoutedEventArgs e)
+      {
+
+      }
+
+      private void Btn_zero_Click(object sender, RoutedEventArgs e)
+      {
+         currentNumber += '0';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
+      }
+
+      private void Btn_dot_Click(object sender, RoutedEventArgs e)
+      {
+         
+      }
+
+      private void Btn_enter_Click(object sender, RoutedEventArgs e)
       {
 
       }
