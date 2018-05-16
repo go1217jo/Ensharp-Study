2c0ad03f5480fa182d89b9fbdb7439fa3f95713d
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-05-17 01:55:39 +0900

    Add printing float type number

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -22,6 +22,10 @@ namespace Calculator
    {
       string currentNumber = "";
       string currentEquation = "";
+      // 가수부
+      string manatissa = "";
+      // 현재 입력되고 있는 숫자가 소수일지 아닐지의 상태
+      bool floatState = false;
 
       Outprocessor outprocessor = new Outprocessor();
       List<Button> numberButtons;
@@ -41,7 +45,8 @@ namespace Calculator
       // 화면에 숫자가 다 출력되도록 텍스트 크기를 조절한다
       public void AdjustTextSize()
       {
-         int increaseRate = currentNumber.Length - 12;
+         int floatPartLength = (manatissa.Length - 1 >= 0) ? manatissa.Length - 1 : 0;
+         int increaseRate = currentNumber.Length + floatPartLength - 12;
          if (increaseRate >= 1 && increaseRate < 4)
             calculationScreen.FontSize = Constant.BASIC_FONT_SIZE - (4.3 - increaseRate * 0.08) * increaseRate;
          // 입력 제한
@@ -61,8 +66,14 @@ namespace Calculator
 
       private void Btn_number_Click(object sender, RoutedEventArgs e)
       {
-         currentNumber += ((Button)sender).Content;
-         calculationScreen.Text = outprocessor.InsertComma(currentNumber);
+         // 소수이면
+         if (floatState)
+            manatissa += ((Button)sender).Content;
+         // 현재 소수가 아니면
+         else
+            currentNumber += ((Button)sender).Content;
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber) + manatissa;
+         
          AdjustTextSize();
       }
 
@@ -84,11 +95,9 @@ namespace Calculator
             case '÷':
                currentEquation += (currentNumber + '/');
                break;
-            case '√':
-               currentEquation += ("√(" + currentNumber + ')');
-               break;
          }
 
+         floatState = false;
          resultScreen.Text = currentEquation;
          currentNumber = "";
       }
@@ -96,6 +105,8 @@ namespace Calculator
       private void Btn_cancel_Click(object sender, RoutedEventArgs e)
       {
          currentNumber = "";
+         manatissa = "";
+         floatState = false;
          calculationScreen.Text = "0";
          AdjustTextSize();
       }
@@ -104,6 +115,8 @@ namespace Calculator
       {
          currentNumber = "";
          currentEquation = "";
+         manatissa = "";
+         floatState = false;
          resultScreen.Text = "";
          calculationScreen.Text = "0";
          AdjustTextSize();
@@ -116,18 +129,39 @@ namespace Calculator
 
       private void Btn_erase_Click(object sender, RoutedEventArgs e)
       {
-         if (currentNumber.Length != 0) {
-            currentNumber = currentNumber.Remove(currentNumber.Length - 1);
-            if (currentNumber.Length == 0)
-               calculationScreen.Text = "0";
-            else
-               calculationScreen.Text = outprocessor.InsertComma(currentNumber);
+         // 현재 소수면
+         if (floatState)
+         {
+            manatissa = manatissa.Remove(manatissa.Length - 1);
+            // 소수점이 지워지면 정수상태
+            if (manatissa.Length == 0)
+               floatState = false;
+            calculationScreen.Text = outprocessor.InsertComma(currentNumber) + manatissa;
          }
+         else
+         {
+            if (currentNumber.Length != 0)
+            {
+               currentNumber = currentNumber.Remove(currentNumber.Length - 1);
+               if (currentNumber.Length == 0)
+                  calculationScreen.Text = "0";
+               else
+                  calculationScreen.Text = outprocessor.InsertComma(currentNumber);
+            }
+         }
+         AdjustTextSize();
       }
 
       private void Btn_dot_Click(object sender, RoutedEventArgs e)
       {
-         
+         if (floatState)
+            return;
+         // 지수부가 없으면 0으로 결정
+         if (currentNumber.Length == 0)
+            currentNumber += '0';
+         manatissa += '.';
+         calculationScreen.Text = outprocessor.InsertComma(currentNumber) + manatissa;
+         floatState = true;
       }
 
       private void Btn_enter_Click(object sender, RoutedEventArgs e)
