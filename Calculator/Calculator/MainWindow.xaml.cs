18d05cf72de3630bd1fb391c3d2d438de7ff743e
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-05-17 14:01:52 +0900

    Add printing when negate and others button click

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -26,6 +26,9 @@ namespace Calculator
       string manatissa = "";
       // 현재 입력되고 있는 숫자가 소수일지 아닐지의 상태
       bool floatState = false;
+      // negate를 위한 문자열, 인덱스
+      string negateNumber = "";
+      int negateStartIndex = 0;
 
       Outprocessor outprocessor = new Outprocessor();
       List<Button> numberButtons;
@@ -40,6 +43,12 @@ namespace Calculator
             numberButtons[idx].Click += new RoutedEventHandler(Btn_number_Click);
          for (int idx = 0; idx < operButtons.Count; idx++)
             operButtons[idx].Click += new RoutedEventHandler(Btn_operation_Click);
+      }
+
+      public void EnableNumberButtons()
+      {
+         for (int idx = 0; idx < numberButtons.Count; idx++)
+            numberButtons[idx].IsHitTestVisible = true;
       }
 
       // 화면에 숫자가 다 출력되도록 텍스트 크기를 조절한다
@@ -58,10 +67,8 @@ namespace Calculator
          }
          else {
             calculationScreen.FontSize = Constant.BASIC_FONT_SIZE;
-            if (!numberButtons[0].IsHitTestVisible) {
-               for (int idx = 0; idx < numberButtons.Count; idx++)
-                  numberButtons[idx].IsHitTestVisible = true;
-            }
+            if (!numberButtons[0].IsHitTestVisible)
+               EnableNumberButtons();
          }
       }
 
@@ -83,7 +90,7 @@ namespace Calculator
          // 현재 입력된 버튼의 연산 문자를 가져옴
          char oper = ((Button)sender).Content.ToString()[0];
          if (currentNumber.Length == 0)
-            currentEquation = currentEquation.Remove(currentEquation.Length - 1);
+            currentEquation = currentEquation.Remove(currentEquation.Length - 3);
 
          // 소수점만 남아있을 경우
          if (manatissa.Length == 1)
@@ -93,13 +100,11 @@ namespace Calculator
          {
             case '+':
             case '-':
-               currentEquation += (currentNumber + manatissa + oper);
+            case '÷':
+               currentEquation += (currentNumber + manatissa + ' ' + oper + ' ');
                break;
             case 'X':
-               currentEquation += (currentNumber + manatissa + 'x');
-               break;
-            case '÷':
-               currentEquation += (currentNumber + manatissa + '/');
+               currentEquation += (currentNumber + manatissa + " x ");
                break;
          }
 
@@ -110,37 +115,74 @@ namespace Calculator
 
          currentNumber = "";
          manatissa = "";
-         for (int idx = 0; idx < numberButtons.Count; idx++)
-            numberButtons[idx].IsHitTestVisible = true;
+         negateNumber = "";
+         EnableNumberButtons();
       }
            
       private void Btn_cancel_Click(object sender, RoutedEventArgs e)
       {
          currentNumber = "";
          manatissa = "";
+         negateNumber = "";
          floatState = false;
          calculationScreen.Text = "0";
          AdjustTextSize();
       }
 
+      // C 버튼
       private void Btn_cancel_all_Click(object sender, RoutedEventArgs e)
       {
          currentNumber = "";
          currentEquation = "";
          manatissa = "";
+         negateNumber = "";
          floatState = false;
          resultScreen.Text = "";
          calculationScreen.Text = "0";
          AdjustTextSize();
+         resultScroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
       }
 
       private void Btn_plusMinus_Click(object sender, RoutedEventArgs e)
       {
-
+         if(currentNumber.Length != 0)
+         {
+            if (currentNumber[0] == '-')
+               currentNumber = currentNumber.Substring(1);
+            else
+               currentNumber = '-' + currentNumber;
+            calculationScreen.Text = outprocessor.InsertComma(currentNumber) + manatissa;
+         }
+         else
+         {
+            if (!calculationScreen.Text.Equals("0"))
+            {
+               if (calculationScreen.Text[0] == '-')
+                  calculationScreen.Text = calculationScreen.Text.Substring(1);
+               else
+                  calculationScreen.Text = '-' + calculationScreen.Text;
+               if (negateNumber.Length == 0)
+               {
+                  negateNumber = "negate(" + calculationScreen.Text + ")";
+                  negateStartIndex = currentEquation.Length;
+                  currentEquation += negateNumber;
+               }
+               else
+               {
+                  negateNumber = "negate(" + negateNumber + ")";
+                  currentEquation = currentEquation.Remove(negateStartIndex) + negateNumber;
+               }
+               
+               resultScreen.Text = currentEquation;
+            }
+         }
       }
 
+      // 백스페이스 버튼
       private void Btn_erase_Click(object sender, RoutedEventArgs e)
       {
+         if (!numberButtons[0].IsHitTestVisible)
+            EnableNumberButtons();
          // 현재 소수면
          if (floatState)
          {
@@ -164,6 +206,7 @@ namespace Calculator
          AdjustTextSize();
       }
 
+      // 점 버튼
       private void Btn_dot_Click(object sender, RoutedEventArgs e)
       {
          if (floatState)
