7f1e3fe46068b326b42804781f96e3fd1b72f39a
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-05-24 10:16:50 +0900

    git init and add login, findID GUI

---
+++ MemberManager/MemberManager/LoginScreen.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemberManager
+{
+   /// <summary>
+   /// LoginScreen.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class LoginScreen : UserControl
+   {
+      public LoginScreen()
+      {
+         InitializeComponent();
+         List<Label> clickableLabels = new List<Label>(new Label[] { findID, findPW, register });
+         for (int idx = 0; idx < clickableLabels.Count; idx++)
+         {
+            clickableLabels[idx].AddHandler(MouseMoveEvent, new RoutedEventHandler(Label_MouseUp));
+            clickableLabels[idx].AddHandler(MouseLeaveEvent, new RoutedEventHandler(Label_MouseLeave));
+            
+         }
+      }
+
+      private void Label_MouseUp(object sender, RoutedEventArgs e)
+      {
+         ((Label)sender).FontWeight = FontWeights.UltraBold;
+         ((Label)sender).FontSize = 20;
+      }
+
+      private void Label_MouseLeave(object sender, RoutedEventArgs e)
+      {
+         ((Label)sender).FontWeight = FontWeights.Regular;
+         ((Label)sender).FontSize = 18;
+      }      
+   }
+}
---
+++ MemberManager/MemberManager/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemberManager
+{
+   /// <summary>
+   /// MainWindow.xaml에 대한 상호 작용 논리
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      LoginScreen login;
+      FindID findID;
+      FindPW findPW;
+      Registration register;
+
+      public MainWindow()
+      {
+         InitializeComponent();
+         init();
+         MainGrid.Children.Add(login);
+      }
+
+      public void init()
+      {
+         login = new LoginScreen();
+         findID = new FindID();
+         findPW = new FindPW();
+         register = new Registration();
+
+         login.findID.AddHandler(MouseDownEvent, new RoutedEventHandler(Label_findID_Click));
+         login.findPW.AddHandler(MouseDownEvent, new RoutedEventHandler(Label_findPW_Click));
+         login.register.AddHandler(MouseDownEvent, new RoutedEventHandler(Label_register_Click));
+      }
+
+      private void Label_findID_Click(object sender, RoutedEventArgs e)
+      {
+         MainGrid.Children.Clear();
+         MainGrid.Children.Add(findID);
+      }
+
+      private void Label_findPW_Click(object sender, RoutedEventArgs e)
+      {
+         MainGrid.Children.Clear();
+         MainGrid.Children.Add(findPW);
+      }
+
+      private void Label_register_Click(object sender, RoutedEventArgs e)
+      {
+         MainGrid.Children.Clear();
+         MainGrid.Children.Add(register);
+      }
+   }
+}
