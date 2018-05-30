2c2ff7753cf9aae1f7be0506a12aa54339798708
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-05-30 14:45:11 +0900

    WindowExplorer init

---
+++ WindowExplorer/WindowExplorer/FileSystem/FolderHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowExplorer.FileSystem
+{
+    class FolderHandler
+    {
+        /// <summary>
+        /// 디렉터리 목록을 반환
+        /// </summary>
+        /// <param name="parentPath"> 이 경로 밑의 폴더 목록을 반환 </param>
+        /// <returns> 디렉터리 목록 </returns>
+        public List<string> GetDirectoryList(string parentPath)
+        {
+            string[] entries = Directory.GetFileSystemEntries(parentPath);
+            foreach (string entry in entries)
+            {
+
+            }
+            return new List<string>();
+        }
+    }
+}
