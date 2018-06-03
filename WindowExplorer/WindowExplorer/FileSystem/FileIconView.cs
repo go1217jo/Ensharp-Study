00e15f439d56eae863baf06310713018ace78558
DESKTOP-HCSHIPN\yeongjoon <go1217jo@naver.com>
2018-06-03 16:48:03 +0900

    feat : Adjust filename's length

--- WindowExplorer/WindowExplorer/FileSystem/FileIconView.cs
+++ WindowExplorer/WindowExplorer/FileSystem/FileIconView.cs
@@ -76,7 +76,7 @@ namespace WindowExplorer.FileSystem
                     image.Width = 64;
                     image.Height = 64;
                     Label filename = new Label();
-                    filename.Content = nameList[pos];
+                    filename.Content = AdjustTextLength(nameList[pos]);
 
                     file.Children.Add(image);
                     file.Children.Add(filename);
@@ -86,6 +86,16 @@ namespace WindowExplorer.FileSystem
                 filePanel.Children.Add(panel);
             }
         }
-
+        private string AdjustTextLength(string filename)
+        {
+            int pos = 0;
+            while (filename.Length > pos)
+            {
+                if (filename.Length - pos >= 11)
+                    filename = filename.Substring(0, pos + 10) + "\n" + filename.Substring(pos + 10);
+                pos += 10;
+            }
+            return filename;
+        }
     }
 }
